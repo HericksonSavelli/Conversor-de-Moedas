a2ca75204278df7b0453207d74fb7eec7f761c18
HericksonSavelli <herickson.savelli11@gmail.com>
2025-04-03 11:17:16 -0300

    acrescentei mais uma moeda Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const currencySelect = document.querySelector(".currency-select")
 const currencySelect1 = document.querySelector(".currency-select1")
 
 
-function convertValues() {
+    async function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyvalueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConvert = document.querySelector(".currency-convert")
 
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL,ARS-BRL").then(response => response.json())    
+    
     const realtoday = 1.00
-    const dolartoday = 5.73
-    const eurotoday = 5.99 //(EUR)"de-DE"
-    const libratoday = 7.26 //(GBP)"en-GB"
-    const PesoArgentino = 0.01 //(ARS)"es-AR"
+    const dolartoday = data.USDBRL.high
+    const eurotoday = data.EURBRL.high 
+    const libratoday = data.GBPBRL.high 
+    const PesoArgentino = data.ARSBRL.high 
+    const bitcoin = data.BTCBRL.high    
 
     if (currencySelect1.value == "Real") {
         currencyvalueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -55,6 +58,14 @@ function convertValues() {
             currency: "ARS"
         }).format(inputCurrencyValue / PesoArgentino)
     }
+     if (currencySelect.value == "Bitcoin") {
+        currencyValueConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BTC",
+            minimumFractionDigits: 8,
+            maximumFractionDigits: 8
+        }).format(inputCurrencyValue / bitcoin)
+    }
 }
 function changeCurrency() {
     const currencyName = document.querySelector(".nomeMoeda")
@@ -85,10 +96,12 @@ function changeCurrency() {
         currencyName.innerHTML = "Peso Argentino"
         currencyImage.src = "./assets/Argentina 1.png"
     }
+     if (currencySelect.value == "Bitcoin") {
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "./assets/bitcoin 1.png"
+    }
 
 }
-
-
 
 currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
